25e5a7432db520d2b4aa979483751648c5ff4d80
Paige Vegna <paigevegna@gmail.com>
2020-01-13 15:55:43 -0500

    basic drivetrain framework

---
+++ src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,28 @@
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Drive extends CommandBase {
+  
+  public Drive() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSystem extends SubsystemBase {
+
+  public static final int[][] D_PORTS = {{0, 1},
+                                         {2, 3},
+                                         {4, 5}};
+
+  public static final int[] E_PORTS = {6, 7};
+  
+  private SpeedControllerGroup left = new SpeedControllerGroup(new Spark(D_PORTS[0][0]), new Spark(D_PORTS[1][0]), new Spark(D_PORTS[2][0]));
+  
+  private SpeedControllerGroup right = new SpeedControllerGroup(new Spark(D_PORTS[0][1]), new Spark(D_PORTS[1][1]), new Spark(D_PORTS[2][1]));
+
+  private Encoder enc = new Encoder(E_PORTS[0], E_PORTS[1]);
+
+  public DriveSystem() {
+
+  }
+
+  public void periodic() {
+    
+  }
+}
