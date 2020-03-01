106a7bd35cf69e1b580b913652da881c99514ab9
Paige Vegna <paigevegna@gmail.com>
2020-03-01 17:10:18 -0500

    auto succ: engage

---
+++ src/main/java/frc/robot/commands/Succ.java
@@ -0,0 +1,32 @@
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Succ extends CommandBase {
+  
+  public Succ() {
+    addRequirements(Robot.is); 
+  }
+
+  
+  @Override
+  public void initialize() {
+  }
+
+  
+  @Override
+  public void execute() {
+    Robot.is.setSpeed(1);
+  }
+
+    @Override
+  public void end(boolean interrupted) {
+    Robot.is.setSpeed(0);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
